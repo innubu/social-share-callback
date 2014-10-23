215669d540fb48bfd4426100076668a9cb94420b
dburgos <david.burgos.risco@gmail.com>
2014-10-23 16:08:27 +0200

    Beta release

--- README.md
+++ README.md
@@ -4,25 +4,27 @@ social-share-callback
 Capture the share callbacks on Facebook, Twitter (tweet) and/or LinkedIn
 
 ## API
-### Load
-(Optional) Async loading of JS APIs. Useful if you don't want to load each one manually in your HTML or want to load on the fly.
-```javascript
-socialShareCallback.load(['facebook','twitter'])
-```
-
-### Facebook
-Set the Share on Facebook callback
-```javascript
-socialShareCallback.facebook(function() {
-	// Succesful Share on Facebook Callback
-})
-```
 
 ### Twitter
 Set the Share on Twitter callback
 ```javascript
 socialShareCallback.twitter(function(tweetEvent) {
 	// Succesful Tweet Callback
+})
+```
+
+### Facebook
+Set the Share on Facebook callback
+```javascript
+// You have to load FB JS with your appId
+// If you are already loading FB with your appId, just skip this step
+socialShareCallback.loadFacebook("307260879379043", function() {
+  console.log("fb loaded");
+});
+
+// Share the URL and set the successful callback
+socialShareCallback.facebook('https://innubu.com', function() {
+	// Succesful Share on Facebook Callback
 })
 ```
 
--- social-share-callback.js
+++ social-share-callback.js
@@ -21,37 +21,32 @@ window.socialShareCallback = (function(window, document){
     t.parentNode.insertBefore(s, t);
   }
 
-  function loadFacebook() {
-    loadScript('http://connect.facebook.net/en_US/all.js');
-  }
-
-  function loadTwitter() {
-    window.twttr = (function (d,s,id) {
-      var t, js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-      js.src="//platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
-      return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
-      }(document, "script", "twitter-wjs"));
-  }
-
-  function loadLinkedIn() {
-    loadScript('//platform.linkedin.com/in.js');
-  }
-
-  function load(medias) {
-    for(var i=0, total=medias.length; i<total; i++) {
-      switch(medias[i]) {
-        case 'facebook':
-          loadFacebook();
-          break;
-        case 'twitter':
-          loadTwitter();
-          break;
-        case 'linkedin':
-          loadLinkedIn();
-          break;
+  function loadFacebook(appId, callback) {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: appId,
+        status: true,
+        cookie: true,
+        xfbml: true,
+        oauth: true
+      });
+    };
+    loadScript("//connect.facebook.net/en_US/all.js", function(){
+      if(callback) {
+        callback();
       }
-    }
+    })
+  }
+
+  function facebook(url, callback) {
+    FB.ui({
+      method: 'share',
+      href: url,
+    }, function(response){
+      if(callback && response && response.post_id) {
+        callback(null, response)
+      }
+    });
   };
 
   function twitter(callback) {
@@ -60,21 +55,16 @@ window.socialShareCallback = (function(window, document){
     });
   };
 
-  function facebook(callback) {
-    FB.Event.subscribe('message.send', function(targetUrl) {
-      callback(targetUrl);
-    });
-  };
-
-  function linkedin(callback, errorCallback) {
+  function linkedin(callback) {
     var li = document.querySelector("script[type='IN/Share']");
-    li['data-sucess'] = callback;
-    li['data-error'] = errorCallback;
+    var successName = callback.name || 'linkedinSuccess';
+    window[successName] = callback;
+    li.setAttribute('data-onsuccess', successName);
   };
 
   return {
-    load: load,
-    twiter: twitter,
+    loadFacebook: loadFacebook,
+    twitter: twitter,
     facebook: facebook,
     linkedin: linkedin
   };
